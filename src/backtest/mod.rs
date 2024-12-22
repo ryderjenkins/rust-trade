6ae0212b1e01ea1e7bceb999897ba68dcc2ee08b
ryderjenkins <ryderjenkins11@gmail.com>
2024-12-23 09:39:04 +1100

    delete unused files

--- src/backtest/mod.rs
+++
@@ -1,6 +0,0 @@
-pub mod engine;
-pub mod strategy;
-pub mod types;
-
-pub use strategy::base::Strategy;
-pub use types::*;
--- src/lib.rs
+++
@@ -1,5 +0,0 @@
-pub mod data;
-pub mod services;
-pub mod config;
-pub mod backtest;
-pub mod ai;
